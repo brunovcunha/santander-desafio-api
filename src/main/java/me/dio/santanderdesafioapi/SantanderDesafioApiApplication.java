517e0035cbafb35f969a0942cabda9ca20cd7faf
brunovcunha <bruno.cunha@estudante.iftm.edu.br>
2024-07-02 14:12:41 -0300

    First commit

---
+++ src/main/java/me/dio/santanderdesafioapi/SantanderDesafioApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.santanderdesafioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDesafioApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SantanderDesafioApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/dio/santanderdesafioapi/SantanderDesafioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santanderdesafioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDesafioApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
